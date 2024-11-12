7df672e46626cf3a687ebfe930e9bdac881d20e7
Suju Nambiar <pb.sujyothi@gmail.com>
2024-11-12 11:40:02 -0500

    original code-new changes

---
+++ src/main/java/sujugitbasics/TestGit1.java
@@ -0,0 +1,15 @@
+package sujugitbasics;
+
+public class TestGit1 {
+
+	public static void main(String[] args) 
+	
+	{
+	System.out.println("Hello");
+	System.out.println("Hi");
+	System.out.println("Welcome!!!");
+	System.out.println("Hello1");
+
+	}
+
+}
